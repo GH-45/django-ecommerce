0e65aed2d0c2f628720af3cffc312547c7c865c4
Ghas <130715143+GH-45@users.noreply.github.com>
2026-03-04 13:35:47 -0300

    build: Configure email backend

--- core/settings.py
+++ core/settings.py
@@ -167,6 +167,17 @@ SIMPLE_JWT: dict[str, Any] = {
 }
 
 
+# Email Backend Configuration
+# https://docs.djangoproject.com/en/6.0/topics/email/
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+
+
 # Custom settings for user verification codes
 VERIFICATION_CODE_LENGTH = 6
 VERIFICATION_CODE_CHARACTERS = "0123456789"
